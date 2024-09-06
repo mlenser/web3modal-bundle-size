d529a8a4e1b431c5324759fd2bf328f5e4d82bcd
Mark Lenser <mlenser@gmail.com>
2024-09-06 08:59:50 +0100

    feat: setup web3modal

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,20 +1,31 @@
 'use client'
 
-import { WagmiProvider } from "wagmi";
+import { type State, WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { wagmiConfig } from "@/app/config/wagmi";
+import { metadata, projectId, wagmiConfig } from "@/app/config/wagmi";
+import { createWeb3Modal } from "@web3modal/wagmi/react";
 
 const queryClient = new QueryClient()
 
+// Create modal
+createWeb3Modal({
+  metadata,
+  wagmiConfig,
+  projectId,
+  enableAnalytics: true // Optional - defaults to your Cloud configuration
+})
+
 export default function Providers({
   children,
+  initialState,
 }: Readonly<{
   children: React.ReactNode;
+  initialState?: State
 }>) {
   return (
-    <WagmiProvider config={wagmiConfig}>
+    <WagmiProvider config={wagmiConfig} initialState={initialState}>
       <QueryClientProvider client={queryClient}>
-      {children}
+        {children}
       </QueryClientProvider>
     </WagmiProvider>
   );
